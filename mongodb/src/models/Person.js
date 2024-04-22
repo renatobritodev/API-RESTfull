c517d3e4a4a4970c188a3e631087a38bdd373404
Renato Brito <dev.renatobrito@gmail.com>
2024-04-21 23:27:44 -0300

    :tada: feat: Initial commit

--- mongodb/src/models/Person.js
+++
@@ -1,7 +0,0 @@
-import 'mongoose'
-
-export const Person = mongoose.model('Person', {
-  name: String,
-  salary: Number,
-  approved: Boolean
-})
--- node/src/server.js
+++
@@ -1,6 +0,0 @@
-import app from './app.js'
-
-const PORT = process.env.PORT || 4000
-const HOST = 'localhost'
-
-app.listen(PORT, () => console.log(`Server running at http://${HOST}:${PORT}`))
